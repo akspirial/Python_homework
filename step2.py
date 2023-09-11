0d08d11a8856acde636c445bc5b3a1307106aadc
akspirial <112485139+akspirial@users.noreply.github.com>
2023-09-11 15:31:51 +0300

    Add files via upload

--- step2.py
+++ step2.py
@@ -1,8 +1,26 @@
 #Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
 #50 -> 1, 2, 4, 8, 16, 32
 
+#def step2():
+ #   try:
+  #      n = int(input('Введите число N: '))
+#
+ #       num1 = []
+  #      num1 = [2**i for i in range(1,n) if 2**i < n]
+   #     if len(num1) !=0:
+    #        return f'{num1}'
+     #   elif len(num1) ==0:
+      #      return f'нужные числа отсутствуют'
+    #except ValueError:
+     #   return 'Вы ввели не число'
+    #except KeyboardInterrupt:
+     #   return "Ведены не все данные"
+#print(step2())
+
+
+
 n = int(input('Введите число N: '))
-num = 1
+num = 0.5
 while num < n:
     if(num < n):
         if (num * 2 > n):
@@ -10,3 +28,4 @@ while num < n:
         else:
             num = num * 2
             print(num)
+
